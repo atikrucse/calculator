86b78100aba17a77e9c2e934598d8965556ebc1f
Atik <atik.rucse@gmail.com>
2019-08-03 13:21:32 +0600

    Add new method for adding

--- calculator1/src/calculator1/Calculator.java
+++ calculator1/src/calculator1/Calculator.java
@@ -2,4 +2,14 @@ package calculator1;
 
 public class Calculator {
 
+	public static void main(String[] args) {
+		
+		System.out.println(" Hello! ");
+		System.out.println(addition(4, 5));
+	}
+	
+	 int addition(int x, int y){
+		
+		return x + y;
+	}
 }
