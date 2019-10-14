af6f26a9f3821b18d9bf02a5fdca61e2309a7c88
Rakib <rakib.uzzaman1232@gmail.com>
2019-10-14 21:43:53 +0600

    new code for cal

--- cal22.c
+++ cal22.c
@@ -1,14 +1,29 @@
-#include<stdio.h>
-
-int multyPlay(int a, int b){
-    int multyPlat=a*b;
-    printf("Multyplaycation= %d\n",multyPlat);
-    retutn 0;
-}
-
-int main(){
-    int firstNumber,secondNumber;
-    scanf("%d %d",&firstNumber,secondNumber);
-    multyPlay(firstNumber,secondNumber);
-
+# include <stdio.h>
+int main() {
+    char operator;
+    double firstNumber,secondNumber;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&firstNumber, &secondNumber);
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber + secondNumber);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber - secondNumber);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber * secondNumber);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
+            break;
+        // operator doesn't match any case constant (+, -, *, /)
+        default:
+            printf("Error! operator is not correct");
+    }
+    
+    return 0;
 }
