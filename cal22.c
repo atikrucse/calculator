28c47eef093061f932a8e50f65f0b394bf5594a3
Rakib <rakib.uzzaman1232@gmail.com>
2019-10-14 21:56:00 +0600

    all code for calculator

--- cal22.c
+++ cal22.c
@@ -21,6 +21,7 @@ int main() {
             printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
             break;
         // operator doesn't match any case constant (+, -, *, /)
+            
         default:
             printf("Error! operator is not correct");
     }
