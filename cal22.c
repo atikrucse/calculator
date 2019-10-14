5cb2ad3880243d5fda80e74d36aa452e9b84b4b4
Atik <atik.rucse@gmail.com>
2019-10-14 21:53:09 +0600

    Multyplacation added

--- cal22.c
+++ cal22.c
@@ -3,12 +3,11 @@
 int multyPlay(int a, int b){
     int multyPlat=a*b;
     printf("Multyplaycation= %d\n",multyPlat);
-    retutn 0;
 }
 
 int main(){
     int firstNumber,secondNumber;
-    scanf("%d %d",&firstNumber,secondNumber);
+    scanf("%d %d",&firstNumber,&secondNumber);
     multyPlay(firstNumber,secondNumber);
 
 }
