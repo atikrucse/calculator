3c17139541b0ad359248fe96b2b91b661666041d
Atik <atik.rucse@gmail.com>
2019-10-14 21:37:48 +0600

    Add multyplay

--- cal22.c
+++ cal22.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
+int multyPlay(int a, int b){
+    int multyPlat=a*b;
+    printf("Multyplaycation= %d\n",multyPlat);
+    retutn 0;
+}
 
 int main(){
-    
-    
+    int firstNumber,secondNumber;
+    scanf("%d %d",&firstNumber,secondNumber);
+    multyPlay(firstNumber,secondNumber);
+
 }
